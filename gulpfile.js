2966d6fa81cddb31e21b52eb0fd25518491f3daf
Kev Comedia <kevcomedia@gmail.com>
2018-02-11 16:47:19 +0800

    Add build step

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,29 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const htmlmin = require('gulp-htmlmin');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 const {reload} = browserSync;
+
+gulp.task('html', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('css', function() {
+  const plugins = [
+    autoprefixer({browsers: ['last 2 versions'], cascade: false}),
+    cssnano(),
+  ]
+  return gulp.src('./src/styles/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/styles'));
+});
 
 // Watch files for changes and reload
 gulp.task('serve', function() {
@@ -17,4 +40,5 @@ gulp.task('serve', function() {
   );
 });
 
+gulp.task('build', ['html', 'css']);
 gulp.task('default', ['serve']);
